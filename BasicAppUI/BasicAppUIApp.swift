5f8f82560045698097d64e04f1a2a300bf0af566
Serdar Altındaş <serdaraltindas03@gmail.com>
2023-10-28 13:00:59 +0300

    FilesAdded

---
+++ BasicAppUI/BasicAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAppUIApp.swift
+//  BasicAppUI
+//
+//  Created by Serdar Altındaş on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct BasicAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
